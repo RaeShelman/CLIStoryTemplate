3b33cc9a510930bdec0d1e773a284f8896b54475
Reversepie314 <ka6johnson.kj@gmail.com>
2023-10-19 11:05:24 -0400

    Commit

--- CLIStoryTemplate/Keon Results 5/Chapter5.swift
+++ CLIStoryTemplate/Keon Results 5/Chapter5.swift
@@ -9,7 +9,7 @@ import Foundation
     func chapterFive() {
         let storyTitle = "Quest" // Apply a simple here
         print("In conclusion, with all the obstables faced, it's now time for our character to discover his true potential.")
-        print("With the discovery of the relic, he will soon understand the importance of learning something new.  Some new stuff.")
+        print("With the discovery of the relic, he will soon understand the importance of learning something new.")
         // Your portion of the story goes here
     }
 
