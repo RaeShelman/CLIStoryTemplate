2fbeb8d9c10ad06049299dc6d13f34e52163e599
luke <lukebanks40@gmail.com>
2023-10-26 09:44:39 -0400

    lukes summary

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -7,4 +7,6 @@ import Foundation
 
 func chapterThree() {
     // Your portion of the story goes here
+   print("Their bond grew stronger as they delved further into the forest, and their belief in their mission remained unshaken. Each member of the group had a unique skill that proved invaluable: there was Alex, the brilliant strategist; Mia, a skilled archer; Jake, a fearless warrior; Lily, a talented healer; and Max, who possessed a deep connection with nature.")
 }
+
