dea555e763d068a3cf83f513b264394d4a02455b
luke <lukebanks40@gmail.com>
2023-10-30 09:00:41 -0400

    add some new things

--- CLIStoryTemplate/Raven - Chapter - 2/Chapter2.swift
+++ CLIStoryTemplate/Raven - Chapter - 2/Chapter2.swift
@@ -8,4 +8,5 @@ import Foundation
 func chapterTwo() {
     // Your portion of the story goes here
     print ("The journey was far from easy. As they ventured deeper into the dense, ancient forest, they encountered a series of traps and challenges. Vicious, thorny vines snaked across their path, attempting to ensnare them, while treacherous quicksand pits threatened to swallow them whole. Yet, the group's courage and unwavering friendship helped them overcome these obstacles.")
+    
 }
