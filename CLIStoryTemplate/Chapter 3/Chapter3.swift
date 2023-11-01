e7e59fe916319ec92b94a60b55efb15acef4f73a
luke <lukebanks40@gmail.com>
2023-11-01 11:41:36 -0400

    new changes 5

--- CLIStoryTemplate/Chapter 3/Chapter3.swift
+++ CLIStoryTemplate/Chapter 3/Chapter3.swift
@@ -15,6 +15,7 @@ class LukesStory{
         self.skillTwo = skillTwo
         self.skillthree = skillthree
         self.skillfour = skillfour
+        
     }
 }
 let therewasabigbeast = 1
@@ -24,10 +25,47 @@ func chapterThree(){
     //
     print("As their journey unfolded through the dense woods, an eerie symphony of strange sounds began to echo around them. Jake's senses heightened, he yelled 'what was that' as he was scanning the surroundings for the source of these unsettling noises. Mia replied 'im not sure but it doesnt sound good'. Mia remained on high alert, her bow poised in readiness, every rustle of leaves triggering her aim.")
     //
-    print("Lily and Max trailed behind Jake and Mia. Max heard and felt something that made him turn around, but saw nothing but bush moving. Becoming suspicous he asked lily 'did you hear that' lily replied 'no', but slowly became afraid of what could happen next.")
+    print("Lily and Max trailed behind Jake and Mia. Max heard and felt something that made him turn around, but saw nothing but bush moving.")
+    print("Jake: What was that? Something's out there.")
+    
+    print("Mia remained on high alert, her bow poised in readiness.")
+    
+    print("Mia: I've got my bow ready. Be on guard, everyone. Keep your eyes peeled.")
+    
+    print("Every rustle of leaves triggered her aim.")
+    
     //
     print("Their cautious advance led them to a glimpse of an mysterious creature, its form shrouded in uncertainty. Straining to discern its true nature, they watched in growing apprehension as it abruptly charged straight at them. Mia yelled 'run', she then reacted swiftly, releasing an arrow that narrowly missed its target. In a matter of seconds, the creature lunged at her, throwing her to the ground.")
     //
+    print("Alex and Max were paralyzed by a surge of fear, gripped by the unimaginable sight before them. This creature was unlike anything they had ever encountered. It possessed the semblance of a colossal wolf, with elongated, razor-sharp claws and a coat of thick, obsidian fur that seemed as sharp as blades.")
+    
+    print("Jake drew his sword, determined to confront the menace.")
+    
+    print("Max tried his best to control the creature but for some reason he could get the beast to stop, this was the first creature he encountered that he couldnt control")
+    
+    print("Max: For some reason I cant control it")
+    
+    print("Jake: Its ok, Stand your ground. We've faced worse.")
+    
+    print("However, his bravery was immediately challenged as the creature leaped towards him, teeth bared and ready to strike.")
+    
+    print("He screamed for help as he uses his sword to try to stop the creature from biting him.")
+    
+    print("In the nick of time, Mia regained her footing, retrieved an arrow, and, with remarkable precision, dispatched the creature with a single shot to the head. Their heart-pounding ordeal concluded as the mysterious creature fell lifeless, sparing Jake from its deadly bite.")
+    
+    print("Mia: Take that, you beast!")
+    
+    print("Their heart-pounding ordeal concluded as the mysterious creature fell lifeless, sparing Jake from its deadly bite.")
+    
+    print("Everyone went to check on jack to see if he was ok. And luckly all he had was a small scratch on his arm")
+    
+    //else if statements below
+    if therewasabigbeast >= 1 {
+        print("They had to fight and kill this big beast")
+    } else {
+        print("There was no beast at all")
+    }
+    
     
     //var listOfTools = ["Backpacks", "Flashlight", "Compass", "Shield"]
     //func chapterThree() {
@@ -38,35 +76,35 @@ func chapterThree(){
     
     //This is for the middle portion of the story
     //This summary is for chapter 3
-    print("Their bond grew stronger as they delved further into the forest, and their belief in their mission remained unshaken. Each member of the group had a unique skill that proved invaluable: there was Alex, the brilliant strategist; Mia, a skilled archer; Jake, a fearless warrior; Lily, a talented healer; and Max, who possessed a deep connection with nature.")
+    //print("Their bond grew stronger as they delved further into the forest, and their belief in their mission remained unshaken. Each member of the group had a unique skill that proved invaluable: there was Alex, the brilliant strategist; Mia, a skilled archer; Jake, a fearless warrior; Lily, a talented healer; and Max, who possessed a deep connection with nature.")
     //
-    print("As their journey unfolded through the dense woods, an eerie symphony of strange sounds began to echo around them.")
-    print("Jake's senses heightened, scanning the surroundings for the source of these unsettling noises.")
-    print("Jake: What was that? Something's out there.")
-    print("Mia remained on high alert, her bow poised in readiness.")
-    print("Mia: I've got my bow ready. Be on guard, everyone. Keep your eyes peeled.")
-    print("Every rustle of leaves triggered her aim.")
+   // print("As their journey unfolded through the dense woods, an eerie symphony of strange sounds began to echo around them.")
+   // print("Jake's senses heightened, scanning the surroundings for the source of these unsettling noises.")
+   // print("Jake: What was that? Something's out there.")
+    //print("Mia remained on high alert, her bow poised in readiness.")
+   // print("Mia: I've got my bow ready. Be on guard, everyone. Keep your eyes peeled.")
+  //  print("Every rustle of leaves triggered her aim.")
     //
-    print("Their cautious advance led them to a glimpse of an enigmatic creature, its form shrouded in uncertainty. Straining to discern its true nature, they watched in growing apprehension as it abruptly charged straight at them. Mia reacted swiftly, releasing an arrow that narrowly missed its target. In a matter of seconds, the creature lunged at her, throwing her to the ground.")
+   // print("Their cautious advance led them to a glimpse of an enigmatic creature, its form shrouded in uncertainty. Straining to discern its true nature, they watched in growing apprehension as it abruptly charged straight at them. Mia reacted swiftly, releasing an arrow that narrowly missed its target. In a matter of seconds, the creature lunged at her, throwing her to the ground.")
     // keep^
-    print("Alex and Max were paralyzed by a surge of fear, gripped by the unimaginable sight before them. This creature was unlike anything they had ever encountered. It possessed the semblance of a colossal wolf, with elongated, razor-sharp claws and a coat of thick, obsidian fur that seemed as sharp as blades.")
+    //print("Alex and Max were paralyzed by a surge of fear, gripped by the unimaginable sight before them. This creature was unlike anything they had ever encountered. It possessed the semblance of a colossal wolf, with elongated, razor-sharp claws and a coat of thick, obsidian fur that seemed as sharp as blades.")
     //keep^
-    print("Jake drew his sword, determined to confront the menace.")
-    print("Jake: Stand your ground, everyone. We've faced worse.")
-    print("However, his bravery was immediately challenged as the creature leaped towards him, teeth bared and ready to strike.")
-    print("He screamed for help as he uses his sword to try to stop the creature from biting him.")
+   // print("Jake drew his sword, determined to confront the menace.")
+    //print("Jake: Stand your ground, everyone. We've faced worse.")
+  //  print("However, his bravery was immediately challenged as the creature leaped towards him, teeth bared and ready to strike.")
+  //  print("He screamed for help as he uses his sword to try to stop the creature from biting him.")
     //
-    print("In the nick of time, Mia regained her footing, retrieved an arrow, and, with remarkable precision, dispatched the creature with a single shot to the head. Their heart-pounding ordeal concluded as the mysterious creature fell lifeless, sparing Jake from its deadly bite.")
-    print("Mia: Take that, you beast!")
-    print("Their heart-pounding ordeal concluded as the mysterious creature fell lifeless, sparing Jake from its deadly bite.")
-    print("Everyone went to check on jack to see if he was ok. And luckly all he had was a small scratch on his arm")
+   // print("In the nick of time, Mia regained her footing, retrieved an arrow, and, with remarkable precision, dispatched the creature with a single shot to the head. Their heart-pounding ordeal concluded as the mysterious creature fell lifeless, sparing Jake from its deadly bite.")
+  //  print("Mia: Take that, you beast!")
+  //  print("Their heart-pounding ordeal concluded as the mysterious creature fell lifeless, sparing Jake from its deadly bite.")
+  //  print("Everyone went to check on jack to see if he was ok. And luckly all he had was a small scratch on his arm")
     
     //else if statements below
-    if therewasabigbeast >= 1 {
-        print("They had to fight and kill this big beast")
-    } else {
-        print("There was no beast at all")
-    }
+ //   if therewasabigbeast >= 1 {
+ //       print("They had to fight and kill this big beast")
+ //   } else {
+ //       print("There was no beast at all")
+ //   }
    
     
     
