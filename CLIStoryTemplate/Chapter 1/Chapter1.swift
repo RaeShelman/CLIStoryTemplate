41f73c9baa6280e4525d2638a17b3ee66d106931
msunko <maryna.sunko@gmail.com>
2023-11-08 08:30:33 -0500

    chapter 1

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -27,22 +27,7 @@ func chapterOne() {
     let libraryHeight = 3
     let storyTitle = "The Quest for Darkness"
 
-    let storyTitle = "The Quest for Darkness"
-    _ = "Alex"
-    _ = "Mia"
-    _ = "Jake"
-    _ = "Lily"
-    _ = "Max"
-    _ = 14
-    _ = 14
-    _ = 14
-    _ = 14
-    _ = 14
-    _ = true
-    _ = 5
-    _ = 2
-    _ = 3
-    
+  
    
     
     let teenagers  = ("Alex","Mia","Jake","Lily", "Max")
@@ -50,19 +35,6 @@ func chapterOne() {
     let yearSleep = ( 100, 500, 1000)
     print ("The shadowLord has awaken fromo his \(yearSleep.2) year sleep to seek revenge on the town of... His intention are further orchasterd on his intent to rule the entire world and fill it with darkness.")
     
-
-
-    func introduction (){
-        print (storyTitle)
-        _ = ["town", "hills", "forest"]
-        _ = [1, 5, 2]
-    }
-        
-    //  print ("  Intrigued and inspired by the heroic stories they read, the teenagers decided to set out on a daring quest to defeat this mythical evil and protect their town. They called themselves The Guardians of Light and embarked on their epic journey armed with newfound knowledge and a determination to save their home.")
-    
-    // The shadowLord has awaken fromo his 1000 year sleep to seek revenge on the town of... His intention are further orchasterd on his intent to rule the entire world and fill it with darkness. 
-    //
-
     func Introduction (){
         print(storyTitle)
         print("Once upon a time, in a small, quiet town nestled amidst rolling hills and ancient forests, there lived a determined young adventurer named Alex.")
@@ -70,11 +42,9 @@ func chapterOne() {
     }
     
     func cityBuilding (housesHeight: Int){
-        //let housesHeight = 2
         if housesHeight >= 2 {
             print ("One sunny morning, while taking a stroll through the town's charming streets, Alex stumbled upon an ancient, forgotten library.The library had stood there for centuries, but few ever ventured inside. Its doors creaked open as Alex pushed their way in.")
-        }
-        else {
+        }else {
             print ("The library was located in the city center")
         }
     }
@@ -89,8 +59,7 @@ func chapterOne() {
     func alt() {
         if altHouses > altLibrary {
             print ("The library was the smallest buuilding in the city")
-        }
-        else {
+        }else {
             print ( "The library was the largest and oldest building in the city.")
         }
     }
